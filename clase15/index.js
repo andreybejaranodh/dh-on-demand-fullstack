f40d1a83b8a2347db99b0930a1c8dcbded953d44
Andrey Bejarano <andreybejarano@gmail.com>
2024-06-18 22:16:25 -0300

    clase 15

---
+++ clase15/index.js
@@ -0,0 +1,41 @@
+// Es espacio reservado en memoria para almacenar cuarquir tipo de de datos, (Numeros, texto, funciones, operaciones, resulados, arreglos, objetos, etc)
+// Es una cajita en la cual yo puedo almacenar lo quiera
+
+// String = Texto - Cadena de caracteres
+// Number = numeros enteros o decimales
+// Boolean = True/False - verdadero o falso
+
+const firstName = 'Andrey';
+const lastName = 'Bejarano';
+const age = 35;
+const isAmerican = true;
+
+// console.log(typeof age);
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof isAmerican);
+
+const numA = 2;
+const numB = 3;
+
+const sum = numA + numB;
+const rest = numA - numB;
+const mult = numA * numB;
+const div = numB / numA;
+
+
+// console.log(sum);
+// console.log(rest);
+// console.log(mult);
+// console.log(div);
+
+const response = 1 + 2 * 2 / 2 - (3 - 2);
+
+const varA = true;
+const varB = false;
+
+// Not A Number
+console.log(varA + varB);
+
+
+// 1 + 2 - 1 = 2
