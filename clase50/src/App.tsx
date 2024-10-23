a8fb98389bff53757d607d87b38b9c0b523e0b6c
Andrey Bejarano <andreybejarano@gmail.com>
2024-10-22 22:18:14 -0300

    clase50

---
+++ clase50/src/App.tsx
@@ -0,0 +1,13 @@
+import {Container, Button} from '@mui/material'
+
+import './App.css'
+
+function App() {  
+  return (
+    <Container>
+      <Button variant="contained">Click me</Button>
+    </Container>
+  )
+}
+
+export default App
